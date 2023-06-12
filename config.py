5c35af093ae8847fc5bd8d02e45423d8f752e6c2
RITESH <108003920+ritesh-0@users.noreply.github.com>
2023-06-13 00:36:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "21288218")
+API_ID = os.environ.get("API_ID", "")
 
-API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
+API_HASH = os.environ.get("API_HASH", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6067754375:AAGVGSGTqaiJ2vziLKcbr43tnyY54rc3p3g") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "Kdramaland_official") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
 DB_NAME = os.environ.get("DB_NAME","User_Data")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://RENAMESNOWPROBOT:RENAMESNOWPROBOT@cluster0.uaf4ivm.mongodb.net/?retryWrites=true")
+DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/f97e69d8f524048e6f908.jpg")
+START_PIC = os.environ.get("START_PIC", "")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6065594762').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
 PORT = os.environ.get("PORT", "8080")
